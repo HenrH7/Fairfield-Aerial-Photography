22a068e6faff5db007b38bd01abc6f5519414a10
HenrH7 <HenrH7@users.noreply.github.com>
2026-09-19 08:21:14 -0400

    Fix portfolio image filename

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -3,7 +3,7 @@ export const portfolioItems = [
         title: "Bedford Audubon",
         category: "Survey",
         location: "Bedford, NY",
-        image: "/img/bedford-main.jpg",
+        image: "/img/bedford-main.JPG",
         content: "A survey of the Bedford Audubon property, looking at the health of the property's shrubland."
     }
     
